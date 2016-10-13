5a3c327f10cf6c3cc05fe569bd20e0dc492fb0f4
kammitama5 <krystal.maughan@gmail.com>
2016-10-13 11:29:59 -0700

    1st commit
    updated comments give indication as to what I was solving for

--- Euler_Sum_01.py
+++ Euler_Sum_01.py
@@ -1,3 +1,5 @@
+#Find sum of all numbers that are multiples of 3 or 5 from 1 through 1000.
+
 def listsum(numList):
     theSum = 0
     for i in numList:
@@ -8,3 +10,4 @@ def listsum(numList):
 numList = range(0, 1000)
 print(listsum(numList))
 #print(listsum([0,1,2,3,4,5,6,7,8,9]))
+#checked solution with numbers 0 through 9 to ensure algorithm worked (base case)
